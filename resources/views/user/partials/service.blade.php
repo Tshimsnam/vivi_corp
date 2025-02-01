95bbf8ced4c8c8f0a36d57564c4b93f52bb27f75
Tshimsnam <manassetshims@gmail.com>
2025-02-01 22:37:15 +0100

    stylisé la page services

--- resources/views/user/partials/service.blade.php
+++ resources/views/user/partials/service.blade.php
@@ -4,12 +4,12 @@
             <h5 class="fw-bold text-primary text-uppercase">Nos Services</h5>
             <h1 class="mb-0">Des solutions personnalisées pour votre entreprise prospère</h1>
         </div>
-        <div class="row g-5">
+        <div class="row g-5 ">
             @foreach ($services as $service)
             <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
-                <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                    <div class="service-icon">
-                        <i class="{{$service->icon}} text-white" ></i>
+                <div <div class="service-item bg-light shadow-lg p-4 d-flex flex-column align-items-center justify-content-center text-center" style="border-radius: 18px;">
+                    <div class="service-icon bg-primary rounded-full p-4 mb-3">
+                        <i class="{{$service->icon}} text-white text-3xl"></i>
                     </div>
                     <h4 class="mb-3">{{$service->title}} </h4>
                     <p class="m-0 text-justify"> {{$service->content}} </p>
